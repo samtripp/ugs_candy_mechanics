9f9dbf0d14a2fe04f89d759b12f5e1dbcd1866ff
Joacim Breiler <joacim@nimlab.se>
2019-04-04 19:57:34 +0200

    Added a spinner when entering the application and it's already running. Added colors to state in footer.

--- src/app/components/select-file/select-file.component.ts
+++ src/app/components/select-file/select-file.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
+import { ISubscription } from "rxjs/Subscription";
 
+import { StatusService } from '../../services/status.service';
 import { FilesService } from '../../services/files.service';
 import { WorkflowManager } from '../../workflow-manager';
 import { FileUtils } from '../../file-utils';
+import { Status } from '../../model/status';
+import { StateEnum } from '../../model/state-enum';
 
 @Component({
   selector: 'app-select-file',
@@ -11,15 +15,22 @@ import { FileUtils } from '../../file-utils';
   styleUrls: ['./select-file.component.scss']
 })
 export class SelectFileComponent implements OnInit, OnDestroy {
-
+  private statusSubscription:ISubscription;
+  private status:Status;
   private fileList:string[];
 
-  constructor(private router: Router, private filesService:FilesService, private workflowManager:WorkflowManager) { }
+  constructor(private router: Router, private filesService:FilesService, private workflowManager:WorkflowManager, private statusService:StatusService) { }
 
   ngOnInit() {
     this.filesService.getWorkspaceFiles().subscribe(fileList => {
       this.fileList = fileList;
     });
+
+    this.status = new Status();
+    this.statusSubscription = this.statusService.getStatus()
+      .subscribe(data => {
+        this.status = data;
+      });
   }
 
   ngOnDestroy() {
@@ -32,5 +43,9 @@ export class SelectFileComponent implements OnInit, OnDestroy {
 
   formatFilename(file:string) : string {
     return FileUtils.convertFilename(file);
+  }
+
+  isReady() : boolean {
+    return this.status !== undefined && (this.status.state == StateEnum.IDLE || this.status.state == StateEnum.ALARM);
   }
 }
